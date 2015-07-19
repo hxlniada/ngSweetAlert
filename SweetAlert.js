7999c3c1c7e4916fd198a4ea64127690ce49dd25
huangxueliang <huangxueliang@baidu.com>
2015-07-19 21:09:33 +0800

    aa

--- SweetAlert.js
+++ SweetAlert.js
@@ -5,48 +5,49 @@
 
 'use strict';
 
-angular.module('oitozero.ngSweetAlert', [])
-.factory('SweetAlert', [ '$rootScope', function ( $rootScope ) {
+angular.module('ngSweetAlert', []).factory('SweetAlert', [
+    '$rootScope', function ($rootScope, $q) {
+        var swal = window.swal;
 
-	var swal = window.swal;
+        // public methods
+        var self = {
 
-	//public methods
-	var self = {
+            swal: function (arg1) {
+                var deferrd = $q.defer();
 
-		swal: function ( arg1, arg2, arg3 ) {
-			$rootScope.$evalAsync(function(){
-				if( typeof(arg2) === 'function' ) {
-					swal( arg1, function(isConfirm){
-						$rootScope.$evalAsync( function(){
-							arg2(isConfirm);
-						});
-					}, arg3 );
-				} else {
-					swal( arg1, arg2, arg3 );
-				}
-			});
-		},
-		success: function(title, message) {
-			$rootScope.$evalAsync(function(){
-				swal( title, message, 'success' );
-			});
-		},
-		error: function(title, message) {
-			$rootScope.$evalAsync(function(){
-				swal( title, message, 'error' );
-			});
-		},
-		warning: function(title, message) {
-			$rootScope.$evalAsync(function(){
-				swal( title, message, 'warning' );
-			});
-		},
-		info: function(title, message) {	
-			$rootScope.$evalAsync(function(){
-				swal( title, message, 'info' );
-			});
-		}
-	};
-	
-	return self;
-}]);
+                swal(arg1, function (isConfirm) {
+                    if (isConfirm) {
+                        deferrd.resolve();
+                    }
+                    else {
+                        deferrd.reject();
+                    }
+                });
+
+                return deferrd.promise;
+            },
+            success: function (title, message) {
+                $rootScope.$evalAsync(function () {
+                    swal(title, message, 'success');
+                });
+            },
+            error: function (title, message) {
+                $rootScope.$evalAsync(function () {
+                    swal(title, message, 'error');
+                });
+            },
+            warning: function (title, message) {
+                $rootScope.$evalAsync(function () {
+                    swal(title, message, 'warning');
+                });
+            },
+            info: function (title, message) {
+                $rootScope.$evalAsync(function () {
+                    swal(title, message, 'info');
+                });
+            }
+
+        };
+        return self;
+    }
+]);
